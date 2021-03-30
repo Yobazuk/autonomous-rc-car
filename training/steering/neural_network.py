6e667a5b6d02d05bfaff9b6238bc94b5784b6048
yobaz <yobazuk@gmail.com>
2021-03-30 18:05:57 +0300

    Imported and prepared data for training

--- training/steering/neural_network.py
+++ training/steering/neural_network.py
@@ -1,18 +1,69 @@
 import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
+#import matplotlib.pyplot as plt
+#import cv2
 
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Convolution2D, Flatten, Dense
 from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+
+def read_image(path, label):
+    image = tf.io.read_file(path)
+    image = tf.image.decode_image(image, channels=3, dtype=tf.float32)
+    return image, label
+
+
+def augment(image, label):
+    # do augmentation
+    return image, label
 
 
 def main():
+
+    dataset_path = r'C:\Users\yobaz\Desktop\Dataset'
+
+    df = []
+
+    for directory in os.listdir(dataset_path):
+        df.append(pd.read_csv(os.path.join(dataset_path, directory, 'data.csv')))
+
+    df = pd.concat(df)
+
+    image_paths = df['image_path'].values
+    steering = df['steering'].values
+
+    for i in range(len(image_paths)):
+        image_paths[i] = os.path.join(dataset_path, image_paths[i].split('/')[-2], image_paths[i].split('/')[-1])
+
+    # input(image_paths)
+
+    images_train, images_val, steering_train, steering_val = train_test_split(image_paths, steering,
+                                                                              test_size=0.2, random_state=10)
+
+    # print(images_train, steering_train, '\n\n\n\n', images_val, steering_val)
+    # input('cont?')
+
+    ds_train = tf.data.Dataset.from_tensor_slices((images_train, steering_train))
+    ds_train = ds_train.map(read_image).map(augment).batch(2)
+
+    ds_validate = tf.data.Dataset.from_tensor_slices((images_val, steering_val))
+    ds_validate = ds_validate.map(read_image).map(augment).batch(2)
+
+    # input_shape=(66, 200, 3)
+
     model = Sequential([
                         # Convolutional feature maps
-                        Convolution2D(24, (5, 5), (2, 2), input_shape=(66, 200, 3), activation='elu'),
+                        Convolution2D(24, (5, 5), (2, 2), input_shape=(480, 640, 3), activation='elu'),
                         Convolution2D(36, (5, 5), (2, 2), activation='elu'),
                         Convolution2D(48, (5, 5), (2, 2), activation='elu'),
                         Convolution2D(64, (3, 3), activation='elu'),
@@ -27,6 +78,8 @@ def main():
                        )
 
     model.compile(Adam(lr=0.0001), loss='mse')
+
+    model.fit(ds_train, epochs=10, validation_data=ds_validate, verbose=1)
 
 
 if __name__ == '__main__':
