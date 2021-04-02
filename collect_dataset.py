c19886f852ec70311266523a45f27182f5337b0c
yobaz <yobazuk@gmail.com>
2021-04-03 00:25:52 +0300

    Implemented new dataset collection method

--- collect_dataset.py
+++ collect_dataset.py
@@ -1,50 +1,90 @@
 from components.motors import Motors
 from components.joystick import Joystick
-from components.camera import Camera
 import pandas as pd
 import os
 import cv2
+import json
 
 
-def save_frame(self, filename, joystick_value):
-    if self.cap.isOpened():
-        ret, frame = self.cap.read()
+with open('config.json') as f:
+    config = json.load(f)
 
-        if ret:
-            frame = cv2.flip(frame, 1)
-            cv2.imwrite(filename, frame)
+PAUSE_DATASET_BTN = config['PAUSE_DATASET_BTN']
+RESUME_DATASET_BTN = config['RESUME_DATASET_BTN']
+DATASET_PATH = os.path.join(os.getcwd(), 'dataset/')
 
-            self.saved_frames.append(filename)
-            self.saved_steering_info.append(joystick_value)
+saved_frames = []
+saved_steering_info = []
+collect_data = False
+collecting_data = False
+turn_sensitivity = config['turn_sensitivity']
+folder_count = 0
 
 
-def save_log(self, path):
-    data = {'Image': self.saved_frames, 'Steering': self.saved_steering_info}
+def save_frame(frame, filename, joystick_value):
+    global saved_frames, saved_steering_info
+
+    cv2.imwrite(filename, frame)
+    saved_frames.append(filename)
+    saved_steering_info.append(joystick_value)
+
+
+def save_log(path):
+    global saved_frames, saved_steering_info
+    data = {'image_path': saved_frames, 'steering': saved_steering_info}
 
     df = pd.DataFrame(data)
-    df.to_csv(path, index=False, header=False)
+    df.to_csv(path, index=False, header=True)
 
-    self.saved_frames = []
-    self.saved_steering_info = []
+    saved_frames = []
+    saved_steering_info = []
 
     print(f'Log {path} saved')
 
 
-def create_new_set(self, path, event, joystick_values):
-    os.mkdir(path)
-    i = 0
+def update_path():
+    global folder_count, DATASET_PATH
 
-    while not event.isSet():
-        self.save_frame(os.path.join(path, str(i) + '.jpg'), joystick_values[config['drive_axis']])
-        i += 1
-
-    print('saved all frames')
-    self.save_log(os.path.join(path, f'data.csv'))
+    while os.path.exists(os.path.join(DATASET_PATH, f'SET{str(folder_count)}')):
+        folder_count += 1
+    return os.path.join(DATASET_PATH, f'SET{str(folder_count)}')
 
 
 def main():
+    global saved_frames, saved_steering_info, collect_data, collecting_data, turn_sensitivity
+
     joystick = Joystick()
-    camera = Camera()
+    cam = cv2.VideoCapture(0)
+    motors = Motors(*config['left_motor'].values(), *config['right_motor'].values())
+
+    path = ''
+    frame_count = 0
+
+    while True:
+        joystick_values = joystick.get_buttons()
+
+        if joystick_values[PAUSE_DATASET_BTN]:
+            collect_data = False
+
+        if joystick_values[RESUME_DATASET_BTN]:
+            collect_data = True
+
+        if collect_data and not collecting_data:
+            path = update_path()
+            os.mkdir(path)
+            collecting_data = True
+
+        if not collect_data and collecting_data:
+            save_log(path)
+            collecting_data = False
+
+        if collecting_data:
+            _, frame = cam.read()
+            save_frame(frame, os.path.join(path, str(frame_count), '.jpg'), joystick_values[config['turn_axis']])
+            frame_count += 1
+
+        motors.move(joystick_values[config['drive_axis']],
+                    (joystick_values[config['turn_axis']] * turn_sensitivity), 0.1)
 
 
 if __name__ == '__main__':
--- components/camera.py
+++ components/camera.py
@@ -12,30 +12,6 @@ class Camera:
     def __init__(self, cap_num=0):
         self._cap_num = cap_num
         self.cap = cv2.VideoCapture(self._cap_num)
-        self.saved_frames = []
-        self.saved_steering_info = []
-
-    def save_frame(self, filename, joystick_value):
-        if self.cap.isOpened():
-            ret, frame = self.cap.read()
-
-            if ret:
-                frame = cv2.flip(frame, 1)
-                cv2.imwrite(filename, frame)
-
-                self.saved_frames.append(filename)
-                self.saved_steering_info.append(joystick_value)
-
-    def save_log(self, path):
-        data = {'Image': self.saved_frames, 'Steering': self.saved_steering_info}
-
-        df = pd.DataFrame(data)
-        df.to_csv(path, index=False, header=False)
-
-        self.saved_frames = []
-        self.saved_steering_info = []
-
-        print(f'Log {path} saved')
 
     def get_frame(self):
         ret, frame = self.cap.read()
@@ -116,17 +92,6 @@ class Camera:
         cap.release()
         out.release()
         cv2.destroyAllWindows()
-
-    def create_new_set(self, path, event, joystick_values):
-        os.mkdir(path)
-        i = 0
-
-        while not event.isSet():
-            self.save_frame(os.path.join(path, str(i) + '.jpg'), joystick_values[config['drive_axis']])
-            i += 1
-
-        print('saved all frames')
-        self.save_log(os.path.join(path, f'data.csv'))
 
     @staticmethod
     def exit():
