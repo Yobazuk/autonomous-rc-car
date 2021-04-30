d885649a4408117ea7f2e46550964135e28c1cb1
yobaz <yobazuk@gmail.com>
2021-05-01 01:34:48 +0300

    Updated camera_angle_adjustment.py

--- training/utilities/camera_angle_adjustment.py
+++ training/utilities/camera_angle_adjustment.py
@@ -16,10 +16,14 @@ def main():
         _, frame = cam.read()
 
         line_height = cv2.getTrackbarPos("Line Height", "Line Height")
+        resized_frame = frame[line_height:frame.shape[0], 0:frame.shape[1]]
+        resized_frame = cv2.resize(resized_frame, (132, 400))
+
         frame = cv2.line(frame, (0, line_height), (frame.shape[1], line_height), (0, 0, 255), 2)
-        frame = cv2.circle(frame, (320, 240), 5, (0, 255, 0), 2)
+        frame = cv2.circle(frame, (int(frame.shape[1]/2), int(frame.shape[0]/2)), 5, (0, 255, 0), 2)
 
         cv2.imshow('frame', frame)
+        cv2.imshow('resized frame', resized_frame)
         cv2.waitKey(1)
 
 
