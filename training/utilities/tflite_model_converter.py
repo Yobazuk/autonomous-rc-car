c8804ffc4d3518f0f417d3bdd570f7ceec7b5814
yobaz <yobazuk@gmail.com>
2021-05-13 17:33:30 +0300

    Removed unnecessary files

---
+++ training/utilities/tflite_model_converter.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# Convert the model
+converter = tf.lite.TFLiteConverter.from_keras_model(load_model(r'../steering/steering_model.h5'))
+tflite_model = converter.convert()
+
+# Save the model.
+with open('../steering/steering_model.tflite', 'wb') as file:
+  file.write(tflite_model)
