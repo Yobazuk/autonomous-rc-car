42fbcf2186e707b617de57420adc660adf4170c9
yobaz <yobazuk@gmail.com>
2021-05-16 18:02:00 +0300

    Added printing the area of ROI

--- training/stop sign detection/stop_sign_detection.py
+++ training/stop sign detection/stop_sign_detection.py
@@ -1,26 +1,28 @@
 import cv2
+from time import sleep
 
 
 sign_cascade = cv2.CascadeClassifier('./stopsign_classifier.xml')
 
-color = 150
+color = (150, 0, 0)
 counter = 0
 cap = cv2.VideoCapture(0)
 
-# video capture loop
 while True:
     _, frame = cap.read()
     frame = cv2.resize(frame, (640, 480))
-    img = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    cv2.imshow('Stop sign detection', img)
+    signs = sign_cascade.detectMultiScale(frame)
+
+    for (x, y, w, h) in signs:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+        area = w*h
+        print(area)
+
     cv2.imshow('original', frame)
     key = cv2.waitKey(1)
     if key == ord('q'):
         break
-
-    print(counter)
 
 cv2.destroyAllWindows()
 cap.release()
